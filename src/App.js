53f1dfa4cfe3819d256e3dfffaeb6fde05d5a272
ant_reyn <antreyn@udel.edu>
2021-07-14 14:31:49 -0400

    used && and ternary to create simple project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import "./App.css";
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to the App!</h1>
+        <button onClick={() => setLoggedIn(!loggedIn)}>
+          {loggedIn ? "Log out" : "Log in"}
+        </button>
+        {loggedIn ? (
+          <h1>Hello Anthony! Welcome.</h1>
+        ) : (
+          <h1>Please log in to continue.</h1>
+        )}
       </header>
     </div>
   );
